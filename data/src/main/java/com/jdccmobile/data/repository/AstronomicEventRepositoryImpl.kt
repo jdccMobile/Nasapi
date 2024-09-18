fdc2976c437d952b9d62ecbdf2c9e1ee10163179
JoseD <jdccMobile@gmail.com>
2024-09-18 20:26:37 +0200

    fix initial error connection

--- data/src/main/java/com/jdccmobile/data/repository/AstronomicEventRepositoryImpl.kt
+++ data/src/main/java/com/jdccmobile/data/repository/AstronomicEventRepositoryImpl.kt
@@ -1,15 +1,12 @@
 package com.jdccmobile.data.repository
 
 import arrow.core.Either
-import arrow.core.flatMap
 import com.jdccmobile.data.local.datasource.AstronomicEventLocalDataSource
-import com.jdccmobile.data.remote.datasource.AstronomicEventRemoteDataSource
 import com.jdccmobile.domain.model.AstronomicEvent
 import com.jdccmobile.domain.model.AstronomicEventId
 import com.jdccmobile.domain.model.MyError
 import com.jdccmobile.domain.repository.AstronomicEventRepository
 import kotlinx.coroutines.flow.Flow
-import java.time.LocalDate
 
 class AstronomicEventRepositoryImpl(
     private val localDataSource: AstronomicEventLocalDataSource,
@@ -31,17 +28,16 @@ class AstronomicEventRepositoryImpl(
         startDate: String,
         endDate: String,
     ): Either<MyError, Unit> {
-        localDataSource.getAstronomicEventList(startDate, endDate).fold(
+        return localDataSource.getAstronomicEventList(startDate, endDate).fold(
             ifLeft = { return Either.Left(it) },
             ifRight = { eventsInDb ->
                 when (eventsInDb.size) {
-                    EVENTS_IN_WEEK -> {}
+                    EVENTS_IN_WEEK -> Either.Right(Unit)
                     NO_EVENTS_IN_WEEK -> requestAndInsertEventsPerWeek.allEvents(startDate, endDate)
                     else -> requestAndInsertEventsPerWeek.specificEvents(startDate, endDate)
                 }
             },
         )
-        return Either.Right(Unit)
     }
 
     override suspend fun switchFavoriteStatus(
