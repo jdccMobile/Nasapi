3256d6b88e53e19fc6c6e2bcd64eb97a7bfd0163
JoseD <jdccMobile@gmail.com>
2024-09-18 07:59:32 +0200

    add favorite icon to card

--- app/src/main/java/com/jdccmobile/nasapi/ui/components/AstronomicEventItem.kt
+++ app/src/main/java/com/jdccmobile/nasapi/ui/components/AstronomicEventItem.kt
@@ -2,18 +2,25 @@ package com.jdccmobile.nasapi.ui.components
 
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jdccmobile.domain.model.AstronomicEventId
@@ -56,13 +63,26 @@ fun AstronomicEventItem(
                     .basicMarquee(),
             )
 
-            Text(
-                text = astronomicEventUi.date.toString(),
-                style = MaterialTheme.typography.bodyMedium,
+            Row(
                 modifier = Modifier
+                    .fillMaxWidth()
                     .padding(horizontal = 16.dp)
                     .padding(bottom = 8.dp),
-            )
+            ) {
+                Text(
+                    text = astronomicEventUi.date.toString(),
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                if (astronomicEventUi.isFavorite) {
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier.size(16.dp)
+                    )
+                }
+            }
         }
     }
 }
